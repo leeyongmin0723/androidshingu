2cf18fe7100db21d1f26a3e840389c115f2a2b6f
Leeyongmin <trex0723@naver.com>
2024-11-18 10:15:53 +0900

    dialog1
    dialog1

--- MainActivity.java
+++ MainActivity.java
@@ -1,99 +1,39 @@
-package com.example.reservation;
+package com.example.dialog2_item;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.Chronometer;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-    Chronometer chronometer;
-    Button btnStart,btnEnd;
-
-    RadioButton rdocal, rdoTime;
-    CalendarView calendar;
-    TimePicker timepicker;
-    TextView tvYear,tvMonth,tvDay, tvHour, tvMinute;
-    int sYear, sMonth, sDay;
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chronometer = findViewById(R.id.chronometer);
-        btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chronometer.start();
-                chronometer.setTextColor(Color.RED);
+                //대화상자 창을 만드시오.
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                //대화상자에 좋아하는 간식 주제로 타이틀을 정하시오.
+                dlg.setTitle("좋아하는 간식은 ?");
+                //종하하는 간식을 담은 배열을 한개 생성하시오.
+                String[] food=new String[]{"딸기","라면","치킨"};
+                //이미지도 추가하시오.
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setItems(food, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        btn.setText(food[i]);
+                    }
+                });
+                dlg.show();
             }
         });
-        rdocal = findViewById(R.id.rdocal);
-        rdoTime = findViewById(R.id.rdoTime);
-        calendar = findViewById(R.id.calender);
-        timepicker = findViewById(R.id.timepicker);
-
-        rdocal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calendar.setVisibility(View.VISIBLE);
-                timepicker.setVisibility(View.INVISIBLE);
-            }
-        });
-        rdoTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calendar.setVisibility(View.INVISIBLE);
-                timepicker.setVisibility(View.VISIBLE);
-            }
-        });
-        btnEnd = findViewById(R.id.btnEnd);
-        tvYear = findViewById(R.id.tvYear);
-        tvMonth = findViewById(R.id.tvMonth);
-        tvDay =findViewById(R.id.tvDay);
-        tvHour =findViewById(R.id.tvHour);
-
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView,
-                                            int iyear, int i1month, int i2day) {
-                sYear = iyear;
-                sMonth = i1month+1;
-                sDay = i2day;
-            }
-        });
-        timepicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker timePicker, int itime, int i1minute) {
-
-            }
-        });
-        btnEnd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chronometer.stop();
-                tvYear.setText(Integer.toString(sYear));
-                tvMonth.setText(Integer.toString(sMonth));
-                tvDay.setText(Integer.toString(sDay));
-                tvHour.setText(Integer.toString(timepicker.getCurrentHour()));
-                tvMinute.setText(Integer.toString(timepicker.getCurrentMinute()));
-            }
-        });
-
-
-
-
-
-
-
-
-
     }
 }
