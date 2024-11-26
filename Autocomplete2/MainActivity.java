60a05117a55177ac0024ec88dda584c754e1c60d
Leeyongmin <trex0723@naver.com>
2024-11-26 13:21:55 +0900

    Add files via upload

---
+++ Autocomplete2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.autocomplete2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+AutoCompleteTextView singleauto;
+MultiAutoCompleteTextView multiauto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        singleauto = findViewById(R.id.autosingle);
+        multiauto = findViewById(R.id.multiauto);
+        //문자열 배열
+        String[] str =new String[]{"shingu-professional","shingu_student","shingu-매점",
+                "friday","friend","alpha","ufc"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_dropdown_item_1line,str);
+        singleauto.setAdapter(adapter);
+        MultiAutoCompleteTextView.CommaTokenizer token =
+                new MultiAutoCompleteTextView.CommaTokenizer();
+        multiauto.setTokenizer(token);
+        multiauto.setAdapter(adapter);
+        }
+    }
